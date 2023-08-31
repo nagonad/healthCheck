1199fb445834d742ef33a527a7e80096529b5d39
Muhammed ali Erdel <nagonad@gmail.com>
2023-08-31 13:11:28 +0200

    first commit

---
+++ HealthCheck/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HealthCheckComponent } from './health-check/health-check.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    FetchDataComponent,
+    NavMenuComponent,
+    HealthCheckComponent,
+  ],
+  imports: [
+    BrowserModule, HttpClientModule, AppRoutingModule, BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+
+})
+export class AppModule { }
